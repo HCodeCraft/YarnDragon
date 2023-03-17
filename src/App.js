0fcfba16998b8b321f2ca0902f749c1e0e104268
Hanna <marmalademarmoset@gmail.com>
2023-03-17 16:38:17 -0700

    Added BrowserRouter to the index.js and added Routes to the App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import Home from './Home'
+import Yarns from './Yarns'
+import NewYarnForm from './NewYarnForm'
+import Yarn from './Yarn'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/yarns' element={<Yarns />}/>
+        <Route path='/yarns/new' element={<NewYarnForm />}/>
+        <Route path='/yarns/:id' element={<Yarn />}/>
+      </Routes>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Welcome to Yarn Dragon!</h1>
+        <hr/>
+        <h2>A place to organize your yarn hoard for maximum crafting enjoyment!</h2>
+    </div>
+  )
+}
+
+export default Home
