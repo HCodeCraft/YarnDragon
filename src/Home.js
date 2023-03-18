509706546f20aec7369696cf7df4551cf49e64c4
Hanna <marmalademarmoset@gmail.com>
2023-03-17 19:37:17 -0700

    added the yarn card css and filled out the component

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Yarn from './Yarn'
+import './Yarn.css'
 
 const Home = () => {
   return (
@@ -6,6 +8,7 @@ const Home = () => {
         <h1>Welcome to Yarn Dragon!</h1>
         <hr/>
         <h2>A place to organize your yarn hoard for maximum crafting enjoyment!</h2>
+        <Yarn name='Yarn name' image='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.FN7EQLjXRtic8oDcLDKZvAHaE9%26pid%3DApi&f=1&ipt=d538a8e652af643f99a74d6af6f7be5fb6501574e8c7c638ab11230692e12f11&ipo=images' body='Info info info'/> //change this to yarns later and map through
     </div>
   )
 }
--- src/Yarn.js
+++ src/Yarn.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
-const Yarn = () => {
+const Yarn = ({name, image, body}) => {
   return (
-    <div>
+    <div className='card-container'>
+        <div className='image-container'>
+            <img src={image} alt='' />
+        </div>
+        <div className='card-content'>
+        <div className='card-title'>
+            <h3>{name}</h3>
+        </div>
+        <div className="card-body">
+            <p>{body}</p>
+        </div>
+
+        <div className='btn'>
+        <button>
+            <a>
+                View more
+            </a>
+        </button>
+        </div>
+
+        </div>
+
 
     </div>
   )
