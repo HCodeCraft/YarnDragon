a41415145d13f355c9386c95bf074b3bede14282
Hanna <marmalademarmoset@gmail.com>
2023-03-18 15:54:00 -0700

    adding YarnLinkcomponent and Yarn.css to github

---
+++ src/YarnLink.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const YarnLink = () => {
+  return (
+    
+    <Link>
+    
+    
+    </Link>
+
+
+
+  )
+}
+
+export default YarnLink
