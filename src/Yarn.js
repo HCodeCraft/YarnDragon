fd7b85f68110665633d92659e6141a8db54a96b8
Hanna <marmalademarmoset@gmail.com>
2023-03-22 10:57:55 -0700

    Adding global state to the Yarn component

--- src/Yarn.js
+++ src/Yarn.js
@@ -1,28 +1,36 @@
 import React from "react";
 import YarnLink from "./YarnLink";
-import { Link, useOutletContext } from "react-router-dom";
+import { Link} from "react-router-dom";
+import { MyConsumer } from "./Context";
 
-const Yarn = ({ name, image, body, weight, id }) => {
-  console.log("yarn id", id);
+// name, image, body, weight, id
+
+const Yarn = (props) => {
+  
+  return (
+    <MyConsumer>
+      {(context) => {
+
+       const yarn = context.yarnsState.find(y => y.id == props.id )
 
   return (
     <div className="card-container">
       <div className="image-container">
-        <img src={image} alt="" />
+        <img src={yarn.image} alt="" />
       </div>
       <div className="card-content">
         <div className="card-title">
-          <h3>{name}</h3>
+          <h3>{yarn.name}</h3>
           <br />
         </div>
         <div className="card-body">
-          <p>Colorway: {body}</p>
+          <p>Colorway: {yarn.weight}</p>
           <br />
-          <p>Weight: {weight}</p>
+          <p>Weight: {yarn.weight}</p>
         </div>
 
         <div className="btn">
-          <Link to={`${id}`}>
+          <Link to={`${yarn.id}`}>
             <button>Show notes/plans</button>
           </Link>
           {/* <YarnLink id={id}/> */}
@@ -31,6 +39,9 @@ const Yarn = ({ name, image, body, weight, id }) => {
         </div>
       </div>
     </div>
+            );
+          }}
+        </MyConsumer>
   );
 };
 
