55ed8c55495e6f1642af6f02c35e215c4fd86d5d
Hanna <marmalademarmoset@gmail.com>
2023-03-23 15:08:18 -0700

    Adding missing components etc to github

---
+++ src/YarnLink.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const YarnLink = ({ yarn }) => {
+  return (
+    <Link to={`/yarns/${yarn.id}`}>
+      <h4>Show Plans </h4>
+    </Link>
+  );
+};
+
+export default YarnLink;
